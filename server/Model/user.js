7be31035a6774083baaeb975dbc163c1bbfbcf1c
yogiiiii26 <61676728+yogiiiii26@users.noreply.github.com>
2020-03-08 12:26:16 +0530

    Add files via upload

--- server/Model/user.js
+++ server/Model/user.js
@@ -13,6 +13,14 @@ const userSchema = mongoose.Schema({
     }
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, checkPassword){
+
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) return checkpassword(err)
+        checkpassword(null, isMatch)
+    })
+}
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
